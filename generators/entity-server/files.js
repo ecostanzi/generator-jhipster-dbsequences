4959a6d60bb70bbaf476ccd51853fe577fde3a94
Enrico Costanzi <enrico.costanzi@intesys.it>
2018-10-05 14:36:23 +0200

    Add custom liquibase changelog

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -1,7 +1,23 @@
 const constants = require('generator-jhipster/generators/generator-constants');
 const SERVER_MAIN_SRC_CUSTOM_DIR = `${constants.MAIN_DIR}java/`;
 
+const INTERPOLATE_REGEX = constants.INTERPOLATE_REGEX;
+const SERVER_MAIN_RES_DIR = constants.SERVER_MAIN_RES_DIR;
+
 const customFiles = {
+    db: [
+        {
+            condition: generator => generator.databaseType === 'sql',
+            path: SERVER_MAIN_RES_DIR,
+            templates: [
+                {
+                    file: 'config/liquibase/changelog/added_entity.xml',
+                    options: { interpolate: INTERPOLATE_REGEX },
+                    renameTo: generator => `config/liquibase/changelog/${generator.changelogDate}_added_entity_${generator.entityClass}.xml`
+                }
+            ]
+        },
+    ],
     server: [
         {
             path: SERVER_MAIN_SRC_CUSTOM_DIR,
